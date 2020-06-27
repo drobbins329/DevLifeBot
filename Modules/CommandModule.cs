ecc5d0cba8bd29ec32694b7894a820b04106b8b0
drobbins329 <drobbins329@gmail.com>
2020-06-26 21:15:54 -0400

    Added Author Command to CommandModule.cs

--- Modules/CommandModule.cs
+++ Modules/CommandModule.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DevLifeBot.Modules
 {
-    class CommandModule
+    class CommandModule : ModuleBase<SocketCommandContext>
     {
+        [Command("author")]
+        [Summary("reply with command Author info")]
+        public async Task btsPicCommand()
+        {
+            try
+            {
+                string message =  "";
+                message += Context.User.Username;
+                message += "\n";
+                message += Context.User.Id;
+                message += "\n";
+                SocketGuildUser author = (SocketGuildUser)Context.User;
+                foreach (SocketRole role in author.Roles)
+                { message += string.Format("\n" + role.Name); }
+                await Context.Message.Channel.SendMessageAsync(message);
+            }
+            catch (Exception e)
+            { Console.WriteLine(e.Message); await Context.Message.Channel.SendMessageAsync("Sorry, something went wrong. :( "); }
+            await Context.Message.DeleteAsync();
+        }
     }
 }
